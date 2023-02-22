1dc703b47ceacb2a67065df4eb0e0f13777876b4
Amritgrg1 <gamrit450@gmail.com>
2023-02-22 10:26:08 +0545

    concept-of-one-dimension-array

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+//        System.out.println("");
+//        int[] x= new int[3];
+//        int[] y ={10, 20, 30};
+//
+//        y[0] = 500;
+
+//        System.out.println(y[0]);
+        int[][] xy= {{10,20}, {20,30}};
+
+        int[] data = {1,10,20,30,40,50,60,70,80,90};
+                for(int element: data){
+                    //code here
+                    System.out.println(element);
+
+                }
+    }
+}
